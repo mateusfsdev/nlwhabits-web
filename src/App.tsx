814f2e56dab6f21ff9f6ae197bf208f8c940eaa3
mateus sant'ana <mateusfdev@outlook.com>
2024-02-13 02:22:42 -0300

    bsc edit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Habit } from "./components/habit";
 export default function App() {
   return (
     <div className="flex h-[100vh] flex-col bg-stone-800 justify-center items-center">
-     <div>
-      <h1 className="font-bold text-3xl items-center text-purple-500">hello</h1>
+ 
+      <h1 className="font-bold text-3xl text-purple-500">hello</h1>
       <Habit completed={33} />
+      <Habit completed={24} />
+      <Habit completed={15} />
+      <Habit completed={34} />
+      <Habit completed={18} />
 
-
-     </div>
     </div>
   )
 }
