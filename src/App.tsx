b4a457a451a3527d642ba6fe07d7e844b29d32f7
mateus sant'ana <mateusfdev@outlook.com>
2024-02-13 02:56:04 -0300

    edit sitax baby

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div className="flex h-[100vh] flex-col bg-stone-800 justify-center items-center">
  
-      <h1 className="font-bold text-3xl text-purple-500">hello</h1>
+      <h1 className="font-bold text-3xl text-purple-500"
+        >hello
+      </h1>
       <Habit completed={33} />
       <Habit completed={24} />
       <Habit completed={15} />
