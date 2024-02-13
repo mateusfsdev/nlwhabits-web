ebd21c462af81c1387b437d15e43637ef52a027e
mateus sant'ana <mateusfdev@outlook.com>
2024-02-13 01:42:48 -0300

    tailwind config 🌸🌸🌸

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Habit } from "./components/Habit";
 
+
 export default function App() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="flex h-[100vh] flex-col bg-stone-800 justify-center items-center">
+      <h1 className="font-bold text-3xl items-center text-purple-500">hello</h1>
       <Habit completed={3}/>
       <Habit completed={5}/>
       <Habit completed={6}/>
--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,9 +1,21 @@
 
+/**
+ * propriedade: informação enviada para modificar o componente
+ * components: reaproveitar codigo ou isolar
+
+ * interface ou type é a descrição para o typescript 
+ * temos que declarar quais as propriedades da fuction Habit
+ * no html sao os atributos como src alt... 
+ * aqui sao chamadas de props
+ */
 
 interface HabitProps {
   completed: number
 }
-
+//acessando ass props como parametro da função e mencionando o padrao HabitProps
 export function Habit(props: HabitProps) {
-  return <h1>{props.completed}</h1>
+  //variavel se chama com chaves
+  return <div className='bg-stone-900/50 rounded m-8 p-6 items-center flex justify-center font-bold size-8 text-purple-500'>{props.completed}</div>
+
+
 }
